4fd67911261e9f2f262b07b051d284cff03ede45
khoinguyen <buikhoinguyen2001@gmail.com>
2022-05-19 15:03:09 +0700

    Add project files.

---
+++ ReferenceIncsharp/Program.cs
@@ -0,0 +1,14 @@
+﻿Main();
+static void Main()
+{
+    int x = 1, y = 2;
+    void ShowValue(int a)
+    {
+        Console.WriteLine($"x = {x}, a = {a}");
+        x = 10;
+    }
+    ShowValue(x);
+    Console.WriteLine($"x = {x}");
+    int? a = null;
+
+}
