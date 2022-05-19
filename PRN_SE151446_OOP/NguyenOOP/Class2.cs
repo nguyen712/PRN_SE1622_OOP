9653c1b0d792fef6fb89337fa2912d11092daa09
khoinguyen <buikhoinguyen2001@gmail.com>
2022-05-19 16:41:32 +0700

    code

---
+++ PRN_SE151446_OOP/NguyenOOP/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN_SE151446_OOP.NguyenOOP
+{
+    public class Person
+    {
+        protected string Name { get; set; }
+        protected Address Address { get; set; }//HAS-(not cách kế thừa)
+        protected DateTime dob { get; set; }
+        public Person()
+        {
+
+        }
+
+        public Person(string name, Address address, DateTime dob)
+        {
+            Name = name;
+            Address = address;
+            this.dob = dob;
+        }
+        public virtual string Display()
+        {
+            return $"[Name = {Name}], Address = {Address}, dob = {dob}";
+        }
+    }
+}
---
+++ PRN_SE151446_OOP/NguyenOOP/employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN_SE151446_OOP.NguyenOOP
+{
+    public class employee: Person
+    {
+        public int ID { get; set; }
+        public double salary { get; set; }
+        public string level { get; set; }
+        
+        public employee()
+        {
+
+        }
+
+        public employee(string name, Address address, DateTime dob, int iD, double salary, string level):base(name, address, dob)
+        {
+            ID = iD;
+            this.salary = salary;
+            this.level = level;
+        }
+        public override string Display()
+        {
+            return $"Id = {ID}, Salary = {salary}, level = {level} " + base.Display();
+        }
+    }
+}
